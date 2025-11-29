944d32ac060613157b11cc9893718df315c21192
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-11-30 02:08:42 +0600

    if only array passed, but need to see maxi of one and zero

--- array/maximum_consecutive_ones_zero.cpp
+++ array/maximum_consecutive_ones_zero.cpp
@@ -40,4 +40,41 @@ int main()
     //  maxi_one(a, 0);
     //  maxi_one(a, 1);
      cout<< maximum(maxi_one(a, 0), maxi_one(a, 1));
+
+    // if pass only array for this then follow
+    // cout<< maxConsecBits(a);
 }
+
+/*
+class Solution {
+  public:
+    int maxConsecBits(vector<int> &arr) {
+        
+        int count0 = 0;
+        int maxi0 = 0; 
+                
+        int count1 = 0;
+        int maxi1 = 0; 
+        
+        
+        for(int i =0; i<arr.size(); i++){
+            if(arr[i] ==1){
+                count1++;
+                count0 =0;
+                maxi1 = max(count1, maxi1);
+                
+            }else{
+                count1=0;
+                count0++; 
+                maxi0 = max(count0, maxi0);
+            }
+        }
+        
+        return max(maxi0, maxi1);
+        
+        
+    }
+};
+
+
+*/
